4682ff24f279c2d5764047148f30d9577d4f1c3b
Arthur Leandro <arthurdocsgo@gmail.com>
2022-01-06 11:06:26 -0300

    CREATE CONTROLLER TaskController

---
+++ code-with-quarkus/src/main/java/mandaloriandevtest/controller/TaskController.java
@@ -0,0 +1,35 @@
+package mandaloriandevtest.controller;
+
+	import java.util.List;
+
+	import javax.inject.Inject;
+	import javax.inject.Singleton;
+
+	import mandaloriandevtest.service.TaskService;
+
+	import io.smallrye.mutiny.Uni;
+
+	@Singleton
+	public class TaskController {
+
+		@Inject
+		TaskService service;
+
+		public Uni<List<String>> getAllKeys() {
+			return service.keys();
+		}
+
+		public String getKey(String key) {
+			return service.get(key);
+		}
+
+		public void insertTO(String key, String value) {
+			service.set(key, value);
+		}
+
+		public Uni<Void> deleteTO(String key) {
+			return service.del(key);
+		}
+
+	}
+
